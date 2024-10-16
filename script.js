a74cc710676470b711c383d39317fea3f891e8c4
SabirPattekari <sabirpattekari413@gmail.com>
2024-10-16 20:28:41 +0800

    made changes in all

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
 // Toggle the hamburger and nav menu
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 // Close the menu when a link is clicked
